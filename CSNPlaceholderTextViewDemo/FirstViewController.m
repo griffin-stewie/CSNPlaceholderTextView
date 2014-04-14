29282ecf77ec4cd2c52d98928db67744b4231646
griffin_stewie <panterathefamilyguy@gmail.com>
2014-04-14 13:22:30 +0900

    Change sample code, using Storyboard to instantiate CSNPlaceholderTextView.

--- CSNPlaceholderTextViewDemo/FirstViewController.m
+++ CSNPlaceholderTextViewDemo/FirstViewController.m
@@ -9,46 +9,10 @@
 #import "FirstViewController.h"
 
 @interface FirstViewController ()
-@property (nonatomic, strong) CSNPlaceholderTextView *textView;
+@property (nonatomic, strong) IBOutlet CSNPlaceholderTextView *textView;
 @end
 
 @implementation FirstViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    self.view.backgroundColor = [UIColor scrollViewTexturedBackgroundColor];
-    [self.view addSubview:self.textView];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-}
-
-- (void)viewDidLayoutSubviews
-{
-    [super viewDidLayoutSubviews];
-    
-    self.textView.frame = CGRectMake(10, 20, 300, 130);
-}
-
-- (CSNPlaceholderTextView *)textView
-{
-    if (_textView == nil) {
-        _textView = [[CSNPlaceholderTextView alloc] initWithFrame:CGRectZero];
-        _textView.placeholder = @"This is placeholder text";
-        _textView.font = [UIFont systemFontOfSize:19];
-    }
-    
-    return _textView;
-}
 
 - (IBAction)hideKeyboardAction:(id)sender
 {
